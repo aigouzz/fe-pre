e39846262a0ceabbf1f36d758343d2adc9d513f1
gxc <1003823149@qq.com>
2022-10-16 00:20:50 +0800

    feat: add es6

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,6 +13,149 @@ module.exports = {
           ],
         sidebar: [
             {
+                title: 'JS基础',
+                path: '/js/1',
+                sidebarDepth: 1,
+                children: [
+                    {
+                        title: 'js中数据类型，存储上区别',
+                        path: '/js/1'
+                    },
+                    {
+                        title: '数组常用方法',
+                        path: '/js/2'
+                    },
+                    {
+                        title: '字符串常用方法',
+                        path: '/js/3'
+                    },
+                    {
+                        title: '类型转换机制',
+                        path: '/js/4'
+                    },
+                    {
+                        title: '==和===区别，分别什么情况使用',
+                        path: '/js/5'
+                    },
+                    {
+                        title: '深拷贝和浅拷贝区别，实现一个深拷贝',
+                        path: '/js/6'
+                    },
+                    {
+                        title: '闭包理解，实现，使用',
+                        path: '/js/7'
+                    },
+                    {
+                        title: '作用域链理解',
+                        path: '/js/8'
+                    },
+                    {
+                        title: 'js原型，原型链，及其特点',
+                        path: '/js/9'
+                    },
+                    {
+                        title: 'js如何实现继承',
+                        path: '/js/10'
+                    },
+                    {
+                        title: 'this对象理解',
+                        path: '/js/11'
+                    },
+                    {
+                        title: '执行上下文和执行栈是什么',
+                        path: '/js/12'
+                    },
+                    {
+                        title: '事件模型',
+                        path: '/js/13'
+                    },
+                    {
+                        title: 'typeof和instanceof区别',
+                        path: '/js/14'
+                    },
+                    {
+                        title: '事件代理及其应用场景',
+                        path: '/js/15'
+                    },
+                    {
+                        title: 'new操作符具体做了什么',
+                        path: '/js/16'
+                    },
+                    {
+                        title: 'ajax原理',
+                        path: '/js/17'
+                    },
+                    {
+                        title: 'bind call apply区别，实现',
+                        path: '/js/18'
+                    },
+                    {
+                        title: '正则表达式理解和应用场景',
+                        path: '/js/19'
+                    },
+                    {
+                        title: '事件循环理解',
+                        path: '/js/20'
+                    },
+                    {
+                        title: 'dom常见操作',
+                        path: '/js/21'
+                    },
+                    {
+                        title: 'bom理解和常见bom对象',
+                        path: '/js/22'
+                    },
+                    {
+                        title: '尾递归理解和应用场景',
+                        path: '/js/23'
+                    },
+                    {
+                        title: '内存泄漏的情况',
+                        path: '/js/24'
+                    },
+                    {
+                        title: '本地存储方式和区别，应用场景',
+                        path: '/js/25'
+                    },
+                    {
+                        title: '函数式编程理解和优缺点',
+                        path: '/js/26'
+                    },
+                    {
+                        title: '函数缓存的实现和应用场景',
+                        path: '/js/27'
+                    },
+                    {
+                        title: '数字精度丢失的问题和如何解决',
+                        path: '/js/28'
+                    },
+                    {
+                        title: '防抖和节流，区别和实现',
+                        path: '/js/29'
+                    },
+                    {
+                        title: '判断元素在可视区域的方式',
+                        path: '/js/30'
+                    },
+                    {
+                        title: '大文件上传断点续传',
+                        path: '/js/31'
+                    },
+                    {
+                        title: '实现上拉加载，下拉刷新',
+                        path: '/js/32'
+                    },
+                    {
+                        title: '单点登录和实现',
+                        path: '/js/33'
+                    },
+                    {
+                        title: 'web常见攻击方式和防御',
+                        path: '/js/34'
+                    },
+                ],
+            },
+            {
                 title: 'ES6',
                 path: '/es6/1',
                 sidebarDepth: 1,
@@ -100,6 +243,10 @@ module.exports = {
                         path: '/react/1'
                     },
                     {
+                        title: 'react的virtual dom和real dom区别',
+                        path: '/react/2'
+                    },
+                    {
                         title: 'react源码',
                         path: '/react/code'
                     },
@@ -141,9 +288,49 @@ module.exports = {
             },
             {
                 title: '算法',
-                path: '/calc/main',
+                path: '/calc/1',
                 sidebarDepth: 1,
                 children: [
+                    {
+                        title: '算法的理解，应用场景',
+                        path: '/calc/1'
+                    },
+                    {
+                        title: '算法时间复杂度和空间复杂度，如何计算',
+                        path: '/calc/2'
+                    },
+                    {
+                        title: '数据结构的理解',
+                        path: '/calc/3'
+                    },
+                    {
+                        title: '栈队列的理解和操作',
+                        path: '/calc/4'
+                    },
+                    {
+                        title: '链表的理解和操作',
+                        path: '/calc/5'
+                    },
+                    {
+                        title: '集合的理解和操作',
+                        path: '/calc/6'
+                    },
+                    {
+                        title: '树结构的理解和操作',
+                        path: '/calc/7'
+                    },
+                    {
+                        title: '堆的理解和操作',
+                        path: '/calc/8'
+                    },
+                    {
+                        title: '图的理解和操作',
+                        path: '/calc/9'
+                    },
+                    {
+                        title: '常见的排序算法和区别',
+                        path: '/calc/10'
+                    },
                     {
                         title: '算法1-3',
                         path: '/calc/first'
