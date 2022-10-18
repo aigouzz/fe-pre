39937566fcc31a6cd0529e7fcd0a806db86a5eb3
gxc <1003823149@qq.com>
2022-10-19 01:24:04 +0800

    feat:add js 12 13

--- docs/.vuepress/components/footStage.vue
+++ docs/.vuepress/components/footStage.vue
@@ -4,3 +4,20 @@
         <img src="logo.png"/>
     </div>
 </template>
+<script>
+export default{
+    mounted() {
+        if(window.performance) {
+            setTimeout(() => {
+                let timing = performance.timing;
+                let dns = timing.connectEnd - timing.connectStart;
+                let ttfb = timing.responseStart - timing.navigationStart;
+                let fp = timing.domInteractive - timing.navigationStart;
+                let dom = timing.domComplete - timing.responseEnd;
+                let lcp = timing.domContentLoadedEventEnd - timing.navigationStart;
+                console.log(`dns寻址时间：${dns}, 首字节到达时间：${ttfb}, 首屏渲染时间：${fp}，dom解析时间：${dom}, 首屏时间：${lcp}`);
+            }, 1000);
+        }
+    }
+}
+</script>
